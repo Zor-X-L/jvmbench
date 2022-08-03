512458b17d3780fdbcf833f35087fee1634412c4
LIU Xiao <liu.xiao.88@foxmail.com>
2022-08-03 22:27:40 +0800

    SmallPt: optimize imports

--- src/main/java/liu/xiao/zor/jvmbench/SmallPt.java
+++ src/main/java/liu/xiao/zor/jvmbench/SmallPt.java
@@ -4,10 +4,8 @@ import java.io.BufferedWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class SmallPt {
 

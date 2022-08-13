54118ea3b1598f89636349e2ad475b70ac0a416b
LIU Xiao <liu.xiao.88@foxmail.com>
2022-08-13 17:10:07 +0800

    Blake2b512: rename class and use @Param to specify message length

--- src/main/java/liu/xiao/zor/jvmbench/Blake2b512.java
+++ src/main/java/liu/xiao/zor/jvmbench/Blake2b512.java
@@ -9,10 +9,11 @@ import java.security.Security;
 import java.util.Random;
 
 @State(Scope.Thread)
-public class Blake2b512Hash1KiB {
+public class Blake2b512 {
 
-    public int size = 1024;
-    public byte[] input = new byte[size];
+    @Param({"102400"})
+    public int blakeMsgLen;
+    public byte[] input;
     public MessageDigest messageDigest;
 
     static {
@@ -22,23 +23,24 @@ public class Blake2b512Hash1KiB {
     @Setup(Level.Iteration)
     public void setup() throws Exception {
         Random random = new SecureRandom();
+        input = new byte[blakeMsgLen];
         random.nextBytes(input);
         messageDigest = MessageDigest.getInstance("BLAKE2B-512", BouncyCastleProvider.PROVIDER_NAME);
     }
 
-    public static byte[] hash(Blake2b512Hash1KiB state) {
+    public static byte[] hash(Blake2b512 state) {
         return state.messageDigest.digest(state.input);
     }
 
     @Benchmark
     @Threads(1)
-    public static byte[] _01_singleThread(Blake2b512Hash1KiB state) {
+    public static byte[] _01_singleThread(Blake2b512 state) {
         return hash(state);
     }
 
     @Benchmark
     @Threads(Threads.MAX)
-    public static byte[] _02_multiThread(Blake2b512Hash1KiB state) {
+    public static byte[] _02_multiThread(Blake2b512 state) {
         return hash(state);
     }
 }
